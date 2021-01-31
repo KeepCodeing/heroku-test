776cfd7dc60b05195642337927a092b5a6582326
KeepCodeing <3350680655@qq.com>
2021-01-31 14:10:13 +0800

    fixed bug

--- dist/server.js
+++ dist/server.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const fs = require('fs');
 const path = require('path');
-const serveStatic = require('serve-static');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(serveStatic(__dirname));
+app.use(express.static(path.resolve(__dirname, './')));
+
+app.listen(port, () => {
+  console.log("port: " + port);
+});
+
+app.get('*', function(req, res) {
+  res.sendFile(path.resolve(__dirname, './index.html'));
+});
